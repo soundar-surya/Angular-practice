077f88c3e14ac8f58e4be04039ecb6f2d0ae5951
surya <bittergourd76@gmail.com>
2021-03-14 01:26:25 +0530

    Implemented structural directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,56 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   //props
+  length = 0;
   password = '';
   name='outputBox';
   note = 'Tap to copy the Password.';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
 
   //methods
+  onChangeLength(value: string){
+    const parsedValue = parseInt(value);
+    if(!isNaN(parsedValue)){
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols(){
+    this.includeSymbols = !this.includeSymbols;
+  }
+
   onButtonClick(){
-    this.password = '@dsgd56&*';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers = '1234567890';
+    const symbols = '!@#$%^&*()';
+    
+    let validChar = '';
+
+    if(this.includeLetters)
+      validChar += letters; 
+    if(this.includeNumbers)
+      validChar += numbers;
+    if(this.includeSymbols) 
+      validChar += symbols;
+
+    let generatedPassword = '';
+    for(let i = 0; i < this.length; i++){
+      console.log(Math.floor(Math.random() * 50));
+      const index = Math.floor(Math.random() * validChar.length);
+      generatedPassword += validChar[index];
+      this.password = generatedPassword;
+    }
+    
   }
 
   getPassword(){
